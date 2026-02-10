661338201a2de5b913ba521762aabda26b6f989e
Alexander Kolmakov <alexander.kolmakov@hwschool.online>
2026-02-10 14:53:40 +0100

    Updated to respect new docs

--- orchestration/analytics/dbt_packages/dbt_external_tables/integration_tests/models/plugins/snowflake/people_alias.sql
+++
@@ -1,4 +0,0 @@
-SELECT
- {{ dbt_utils.star(from=ref('people'), except=['email']) }},
-  email as email_alias
-FROM {{ ref('people') }}
--- orchestration/analytics/dbt_packages/dbt_external_tables/integration_tests/models/plugins/snowflake/people_expression.sql
+++
@@ -1,4 +0,0 @@
-SELECT
- {{ dbt_utils.star(from=ref('people')) }},
-  split_part(email, '@', 2) as email_domain
-FROM {{ ref('people') }}
